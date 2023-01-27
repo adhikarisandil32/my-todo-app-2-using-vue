1393b1c880d973f86edf6dbc3b981e480db6844e
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-01-27 11:11:50 +0545

    m

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -4,7 +4,9 @@
         <ul>
             <li v-for="eachTodo in todoLists" :key="eachTodo.id">
                 {{eachTodo.title}}
-                <button @click="removeClicked(todoLists, key)">Remove</button>
+                <!-- <button @click="removeClicked(todoLists, key)">Remove</button> -->
+                
+                <button @click="removeClicked(key)">Remove</button>
             </li>
         </ul>
     </section>
@@ -12,11 +14,18 @@
 
 <script setup>
     /* eslint-disable */
-    defineProps({todoLists:Object}) //or defineProps(['todoLists'])
 
-    let removeClicked = (todoLists, removeFrom) => {
-        todoLists.splice(removeFrom, 1)
+    const props = defineProps({todoLists:Object}) //or defineProps(['todoLists'])
+    
+    //function for button with event action removeClicked(key)
+    let removeClicked = (removeFrom) => {
+        props.todoLists.splice(removeFrom, 1)
     }
+
+    //function for button with event action removeClicked(todoLists, key)
+    /* let removeClicked = (todoLists, removeFrom) => {
+        todoLists.splice(removeFrom, 1)
+    } */
 </script>
 
 <style>
